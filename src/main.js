b52134fd6fb77af54f491652a345bda39c19ae6c
jonathaneche <jonathan.eche@gmail.com>
2023-05-22 19:38:43 +0200

    configuracion de sass con acceso global desde el vue.config.js, e inst

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+//Importando Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css"   //Para importar los estilos css
+import "bootstrap/dist/js/bootstrap.js"         //Para importar los js
+
 createApp(App).use(router).mount('#app')
