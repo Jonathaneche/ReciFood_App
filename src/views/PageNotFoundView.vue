f500d1ddd3d98b2bbb406639a08e77c6febcb2dc
jonathaneche <jonathan.eche@gmail.com>
2023-06-04 15:27:35 +0200

    PageNotFound agregado y configurado en el router, faltan los estilos

---
+++ src/views/PageNotFoundView.vue
@@ -0,0 +1,14 @@
+<template>
+      <NavBar />
+    <div class="container flex flex-col items-center mt-5">
+        <h1 class="text-3xl mb-3">Sorry, this page does not extist!</h1>
+        <RouterLink class="btn btn-light-green"
+        :to="{name:'home'}"
+        > Back to Home</RouterLink>
+    </div>
+</template>
+
+<script setup>
+import NavBar from '@/components/NavBar.vue'
+
+</script>
