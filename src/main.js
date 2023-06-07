605149a606017b80c9fc2ef1b94213eea494a0c8
jonathaneche <jonathan.eche@gmail.com>
2023-06-07 21:35:56 +0200

    instalacion y configuracion de Firebase en el proyecto

--- src/main.js
+++ src/main.js
@@ -8,7 +8,23 @@ import { useMealsStore } from "../src/store/mealsStore";
 import "bootstrap/dist/css/bootstrap.min.css"; //Para importar los estilos css
 import "bootstrap/dist/js/bootstrap.js"; //Para importar los js
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 const pinia = createPinia();
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDK3Hn0ixGoorKi0hZv_93UNc3MEdSftYw",
+  authDomain: "recifood-19188.firebaseapp.com",
+  projectId: "recifood-19188",
+  storageBucket: "recifood-19188.appspot.com",
+  messagingSenderId: "37161104911",
+  appId: "1:37161104911:web:4ebeaafee8eff6e9da957a",
+};
+initializeApp(firebaseConfig);
 
 createApp(App)
   .use(pinia)
